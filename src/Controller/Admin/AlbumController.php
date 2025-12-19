e48a1688fe0f789e0d1562407df67d5489dd0fe7
Marine Kouadio <marinekouadio@gmail.com>
2025-12-19 15:20:16 +0100

    chore: migration vers Symfony 6.4 LTS

--- src/Controller/Admin/AlbumController.php
+++ src/Controller/Admin/AlbumController.php
@@ -6,34 +6,40 @@ use App\Entity\Album;
 use App\Entity\Media;
 use App\Form\AlbumType;
 use App\Form\MediaType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Contrôleur pour gérer les albums dans l'interface d'administration
+ */
 
 class AlbumController extends AbstractController
 {
-    /**
-     * @Route("/admin/album", name="admin_album_index")
-     */
-    public function index()
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {}
+
+    #[Route('/admin/album', name: 'admin_album_index')]
+    public function index(): Response
     {
-        $albums = $this->getDoctrine()->getRepository(Album::class)->findAll();
+        $albums = $this->entityManager->getRepository(Album::class)->findAll();
 
         return $this->render('admin/album/index.html.twig', ['albums' => $albums]);
     }
 
-    /**
-     * @Route("/admin/album/add", name="admin_album_add")
-     */
-    public function add(Request $request)
+    #[Route('/admin/album/add', name: 'admin_album_add')]
+    public function add(Request $request): Response
     {
         $album = new Album();
         $form = $this->createForm(AlbumType::class, $album);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->persist($album);
-            $this->getDoctrine()->getManager()->flush();
+            $this->entityManager->persist($album);
+            $this->entityManager->flush();
 
             return $this->redirectToRoute('admin_album_index');
         }
@@ -41,17 +47,20 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/add.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/album/update/{id}", name="admin_album_update")
-     */
-    public function update(Request $request, int $id)
+    #[Route('/admin/album/update/{id}', name: 'admin_album_update')]
+    public function update(Request $request, int $id): Response
     {
-        $album = $this->getDoctrine()->getRepository(Album::class)->find($id);
+        $album = $this->entityManager->getRepository(Album::class)->find($id);
+
+        if (!$album) {
+            throw $this->createNotFoundException('Album not found');
+        }
+
         $form = $this->createForm(AlbumType::class, $album);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $this->entityManager->flush();
 
             return $this->redirectToRoute('admin_album_index');
         }
@@ -59,14 +68,17 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/update.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/album/delete/{id}", name="admin_album_delete")
-     */
-    public function delete(int $id)
+    #[Route('/admin/album/delete/{id}', name: 'admin_album_delete')]
+    public function delete(int $id): Response
     {
-        $media = $this->getDoctrine()->getRepository(Album::class)->find($id);
-        $this->getDoctrine()->getManager()->remove($media);
-        $this->getDoctrine()->getManager()->flush();
+        $album = $this->entityManager->getRepository(Album::class)->find($id);
+
+        if (!$album) {
+            throw $this->createNotFoundException('Album not found');
+        }
+
+        $this->entityManager->remove($album);
+        $this->entityManager->flush();
 
         return $this->redirectToRoute('admin_album_index');
     }
